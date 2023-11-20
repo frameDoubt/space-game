69a24f0c88a277c71fe22139780b03a3ea1043b0
Brandon <brandon.w.wright4@gmail.com>
2023-11-20 09:35:36 -0800

    add spawner script

---
+++ Characters/Scripts/Spawner.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var projectileScene = preload("res://Characters/Components/Projectile.tscn")
+
+func _ready():
+	pass
+
+func _unhandled_input(event):
+	if (event.is_action_pressed("fire")):
+		var projectile = projectileScene.instantiate()
+		get_parent().add_child(projectile)
+		projectile.global_position = self.global_position
+		print("fire")
