83b9d5305c30a8244d6c2f6195dec403babea50a
Brandon <brandon.w.wright4@gmail.com>
2023-11-23 20:19:09 -0800

    add collsion return

--- Characters/Scripts/Projectile.gd
+++ Characters/Scripts/Projectile.gd
@@ -7,4 +7,6 @@ func _ready():
 	pass
 
 func _process(delta):
-	move_and_collide(transform.basis_xform(Vector2.UP) * speed * delta)
+	var collision = move_and_collide(transform.basis_xform(Vector2.UP) * speed * delta)
+	if collision:
+		print("I collided with ", collision.get_collider().name)
